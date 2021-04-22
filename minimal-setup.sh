c07569317f4eb104a4d26d398c63f15700c0c2ab
BlackCursive <28059715+BlackCursive@users.noreply.github.com>
2021-04-21 20:26:40 -0400

    Add files via upload

---
+++ minimal-setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo "==================================="
+echo " --- Ubuntu / KDE Setup Script  ---"
+echo "==================================="
+echo "========= Minimal Install ========="
+
+sleep 3
+clear
+sudo apt update -y
+sudo apt install -y stacer curl git dolphin brave-browser snapd tasksel sddm gimp software-properties-common ubuntu-restricted-extras synaptic pavucontrol gparted p7zip-full p7zip-rar gpart 
+sudo apt update -y
+
+sudo dpkg-reconfigure sddm
+sudo tasksel install kubuntu-desktop
+sudo apt update -y
+
+sudo snap install code --classic && sudo snap install bitwarden mailspring joplin-james-carroll
+sudo apt update -y
+
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update -y
+sudo apt install python3.9
+
+sudo apt full-upgrade -y --allow-downgrades
+sudo apt autoremove -y --purge
+sudo apt autoclean -y
