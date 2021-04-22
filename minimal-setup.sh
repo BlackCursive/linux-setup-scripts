b2a78ea555513773aa506d5f5bdcf7c9ecffdbd3
BlackCursive <28059715+BlackCursive@users.noreply.github.com>
2021-04-22 08:27:47 -0400

    Update minimal-setup.sh

--- minimal-setup.sh
+++ minimal-setup.sh
@@ -7,10 +7,13 @@ echo "========= Minimal Install ========="
 sleep 3
 clear
 sudo apt update -y
+sudo apt -y install kde-plasma-desktop plasma-nm
+sudo apt update -y
+
 sudo apt install -y stacer curl git dolphin brave-browser snapd tasksel sddm gimp software-properties-common ubuntu-restricted-extras synaptic pavucontrol gparted p7zip-full p7zip-rar gpart 
 sudo apt update -y
 
-sudo apt -y install kde-plasma-desktop plasma-nm
+sudo snap install code --classic && sudo snap install bitwarden mailspring joplin-james-carroll
 sudo apt update -y
 
 sudo apt -y zsh
@@ -18,9 +21,6 @@ chsh -s /usr/bin/zsh
 sudo apt -y install powerline fonts-powerline zsh-theme-powerlevel9k zsh-syntax-highlighting
 echo "source /usr/share/powerlevel9k/powerlevel9k.zsh-theme" >> ~/.zshrc
 echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
-sudo apt update -y
-
-sudo snap install code --classic && sudo snap install bitwarden mailspring joplin-james-carroll
 sudo apt update -y
 
 sudo add-apt-repository ppa:deadsnakes/ppa
